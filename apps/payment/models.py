6d1081e2c3eeede17547a4fcc2bd5d0fae035905
Shahriar Rahman <shahriar.rahman0709@gmail.com>
2024-11-25 08:04:37 +0600

    payment migrations renewed

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -18,7 +18,7 @@ class Payment(CustomModel):
     final_amount = models.FloatField(blank=True, null=True)
     is_paid = models.BooleanField(blank=True, null=True, default=False)
     is_refund = models.BooleanField(blank=True, null=True, default=False)
-    payslip = models.FileField(blank=True, null=True) # If paid in cash
+    payment_payslip = models.FileField(blank=True, null=True) # If paid in cash
 
     class Meta:
         db_table = 'payment'
